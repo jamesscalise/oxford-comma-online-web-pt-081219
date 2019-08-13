5aafd934b30afa6e2a2c489af02e23866d76d9eb
James Scalise <jamescscalise@gmail.com>
2019-08-13 19:21:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   string = ""
-  array.each_with {|word, index|
+  array.each_with_index {|word, index|
     if index + 1 == array.length ? string << "and #{word}." : string << "#{word}, "
   }
   return string
